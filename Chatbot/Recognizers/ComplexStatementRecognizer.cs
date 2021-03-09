76969e828c5ee7befc92ba8a3c4c8c579af38462
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-03-09 22:37:42 +0100

    Fix configuration in complex recognizer

--- Chatbot/Recognizers/ComplexStatementRecognizer.cs
+++ Chatbot/Recognizers/ComplexStatementRecognizer.cs
@@ -6,44 +6,44 @@ using System.Threading.Tasks;
 
 namespace Chatbot.Recognizers
 {
-  public class ComplexStatementRecognizer : IRecognizer
-  {
-    private readonly LuisRecognizer recognizer;
-
-    public ComplexStatementRecognizer(IConfiguration configuration)
+    public class ComplexStatementRecognizer : IRecognizer
     {
-      var luisIsConfigured = !string.IsNullOrEmpty(configuration["Luis:Simple:AppId"]) && !string.IsNullOrEmpty(configuration["Luis:APIKey"]) && !string.IsNullOrEmpty(configuration["Luis:APIHostName"]);
-      if (luisIsConfigured)
-      {
-        var luisApplication = new LuisApplication(
-            configuration["Luis:Simple:AppId"],
-            configuration["Luis:APIKey"],
-            "https://" + configuration["Luis:APIHostName"]);
-        // Set the recognizer options depending on which endpoint version you want to use.
-        // More details can be found in https://docs.microsoft.com/en-gb/azure/cognitive-services/luis/luis-migration-api-v3
-        var recognizerOptions = new LuisRecognizerOptionsV3(luisApplication)
+        private readonly LuisRecognizer recognizer;
+
+        public ComplexStatementRecognizer(IConfiguration configuration)
         {
-          PredictionOptions = new Microsoft.Bot.Builder.AI.LuisV3.LuisPredictionOptions
-          {
-            IncludeInstanceData = true,
-          }
-        };
+            var luisIsConfigured = !string.IsNullOrEmpty(configuration["Luis:Complex:AppId"]) && !string.IsNullOrEmpty(configuration["Luis:APIKey"]) && !string.IsNullOrEmpty(configuration["Luis:APIHostName"]);
+            if (luisIsConfigured)
+            {
+                var luisApplication = new LuisApplication(
+                    configuration["Luis:Complex:AppId"],
+                    configuration["Luis:APIKey"],
+                    "https://" + configuration["Luis:APIHostName"]);
+                // Set the recognizer options depending on which endpoint version you want to use.
+                // More details can be found in https://docs.microsoft.com/en-gb/azure/cognitive-services/luis/luis-migration-api-v3
+                var recognizerOptions = new LuisRecognizerOptionsV3(luisApplication)
+                {
+                    PredictionOptions = new Microsoft.Bot.Builder.AI.LuisV3.LuisPredictionOptions
+                    {
+                        IncludeInstanceData = true,
+                    }
+                };
 
-        recognizer = new LuisRecognizer(recognizerOptions);
-      }
+                recognizer = new LuisRecognizer(recognizerOptions);
+            }
+        }
+
+        // Returns true if luis is configured in the appsettings.json and initialized.
+        public virtual bool IsConfigured => recognizer != null;
+
+        public async Task<RecognizerResult> RecognizeAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            return await recognizer.RecognizeAsync(turnContext, cancellationToken);
+        }
+
+        public async Task<T> RecognizeAsync<T>(ITurnContext turnContext, CancellationToken cancellationToken) where T : IRecognizerConvert, new()
+        {
+            return await recognizer.RecognizeAsync<T>(turnContext, cancellationToken);
+        }
     }
-
-    // Returns true if luis is configured in the appsettings.json and initialized.
-    public virtual bool IsConfigured => recognizer != null;
-
-    public async Task<RecognizerResult> RecognizeAsync(ITurnContext turnContext, CancellationToken cancellationToken)
-    {
-      return await recognizer.RecognizeAsync(turnContext, cancellationToken);
-    }
-
-    public async Task<T> RecognizeAsync<T>(ITurnContext turnContext, CancellationToken cancellationToken) where T : IRecognizerConvert, new()
-    {
-      return await recognizer.RecognizeAsync<T>(turnContext, cancellationToken);
-    }
-  }
 }

119e7dcd452b658c8631608e63b24dae7a49ba81
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-03-10 08:35:49 +0100

    Create dialog skeletons

---
+++ Chatbot/Dialogs/ComplexParsingDialog.cs
@@ -0,0 +1,27 @@
+﻿using Chatbot.Recognizers;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Extensions.Logging;
+
+namespace Chatbot.Dialogs
+{
+    public class ComplexParsingDialog : CancelAndHelpDialog
+    {
+        private readonly ComplexStatementRecognizer complexRecognizer;
+        protected readonly ILogger logger;
+
+        public ComplexParsingDialog(ComplexStatementRecognizer complexRecognizer, ILogger<ComplexParsingDialog> logger)
+            : base(nameof(ComplexParsingDialog))
+        {
+            this.complexRecognizer = complexRecognizer;
+            this.logger = logger;
+
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
+            {
+            }));
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+    }
+}
---
+++ Chatbot/Dialogs/SimpleParsingDialog.cs
@@ -0,0 +1,27 @@
+﻿using Chatbot.Recognizers;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Extensions.Logging;
+
+namespace Chatbot.Dialogs
+{
+    public class SimpleParsingDialog : CancelAndHelpDialog
+    {
+        private readonly SimpleStatementRecognizer simpleRecognizer;
+        protected readonly ILogger logger;
+
+        public SimpleParsingDialog(SimpleStatementRecognizer simpleRecognizer, ILogger<SimpleParsingDialog> logger)
+            : base(nameof(SimpleParsingDialog))
+        {
+            this.simpleRecognizer = simpleRecognizer;
+            this.logger = logger;
+
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
+            {
+            }));
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+    }
+}

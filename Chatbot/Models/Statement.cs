99d30dd16e668a596e836d28864c06e1d0153d50
Márton Herdey <nadmherdey@gmail.com>
2021-04-14 13:55:42 +0200

    Luis modell leváltása
    Leválttottam a Luis modelleket és kiszerveztem a ComplexModel _Entities osztályát, hogy tudjuk használni máshol is őket.

---
+++ Chatbot/Models/Statement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatbot.Models
+{
+    public class Statement
+    {
+        public string Text { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Property { get; set; }
+
+        public string[] Value { get; set; }
+
+        public bool Negated { get; set; }
+
+        public bool Smaller { get; set; }
+
+        public bool Bigger { get; set; }
+
+        public bool MultipleValues { get; set; }
+
+        public bool DateValues { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
